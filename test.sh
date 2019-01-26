6cacab2efa63c2e72331f1f1398f06515eecb94a
srikanths <ssrikanth314@gmail.com>
2019-01-26 16:19:38 +0530

    modifed

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ echo 'Hi Banglaore'
 echo 'This is modified'
 echo 'Hi ecity'
 echo 'Hi not modified'
-echo "this is second modified in git"
+echo "this is second modified in git"'
+echo 'this is last line modified in git'
